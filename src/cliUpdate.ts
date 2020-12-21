eb76bbb202a1989ea2e76fd938fed874c3119993
박현우 <phu54321@naver.com>
2020-12-21 13:53:10 +0900

    feat: skip auto-update on local development cycle

--- src/cliUpdate.ts
+++ src/cliUpdate.ts
@@ -4,6 +4,15 @@ import { getStdout, runCommand } from './utils/execCommand'
 export async function shouldUpdateCli (): Promise<boolean> {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const packageJson = require('../package.json')
+
+  // Check if this package is installed via `npm run link`
+  // if this is local development, skip auto-update check.
+  const globalLinkedPackages = Object.keys(JSON.parse(await getStdout('npm ls -g --depth=0 --link=true --json=true')).dependencies)
+  if (globalLinkedPackages.indexOf(packageJson.name) !== -1) {
+    console.debug('This package is installed locally via \'npm run link\'. Skipping auto-update check')
+    return false
+  }
+
   const cliVersion = packageJson.version
   const latestVersion = (await getStdout(`npm show ${packageJson.name} version`)).trim()
 
